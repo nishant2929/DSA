6d57a59e75eb643ca047af52fd7a6078c3caa338
nishant2929 <83642998+nishant2929@users.noreply.github.com>
2022-03-19 15:22:05 +0530

    Time: 9 ms (15.07%), Space: 8.6 MB (38.59%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -4,6 +4,7 @@ public:
         vector<int> res;
         
         for(int i=1; i<=nums.size(); i++){
+            //accumulate function means sun of begin to end then stor in oth place
             res.push_back(accumulate(nums.begin(), (nums.begin()+i), 0));
         }
         return res;
