9fd48a17f4afaad07a26249dd26d7732cf71f358
nishant2929 <83642998+nishant2929@users.noreply.github.com>
2022-04-04 08:10:07 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (38.30%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int> res;
-        
-        for(int i=1; i<=nums.size(); i++){
-            //accumulate function means sun of begin to end then stor in oth place
-            res.push_back(accumulate(nums.begin(), (nums.begin()+i), 0));
+        int sum=nums[0];
+        for(int i=1;i<nums.size();i++){
+            sum=sum+nums[i];
+            nums[i]= sum;
+            
         }
-        return res;
+        return nums;
     }
 };
