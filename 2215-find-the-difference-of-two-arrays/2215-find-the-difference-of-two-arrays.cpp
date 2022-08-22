9e9600cfd0b29d47b21e1df6a1cf313d6955a872
nishant2929 <83642998+nishant2929@users.noreply.github.com>
2022-08-23 04:47:12 +0530

    Time: 87 ms (59.55%), Space: 33 MB (36.18%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -8,18 +8,34 @@ public:
         for(auto it:nums2){
             s2.insert(it);
         }
+        vector<int> ans1;
+        vector<int> ans2;
         
-        vector<vector<int>> ans(2);
-        for(auto it: s1){
-            if(s2.count(it)==0)
-                ans[0].push_back(it);
+//         vector<vector<int>> ans(2);
+//         for(auto it: s1){
+//             if(s2.count(it)==0)
+//                 ans[0].push_back(it);
+//         }
+        
+//         for(auto it: s2){
+//             if(s1.count(it)==0)
+//                 ans[1].push_back(it);
+//         }
+        
+//         return ans;
+//     }
+        
+        for(auto it : s1){
+            if(s2.find(it)==s2.end())
+                ans1.push_back(it);
         }
         
-        for(auto it: s2){
-            if(s1.count(it)==0)
-                ans[1].push_back(it);
+          
+        for(auto it : s2){
+            if(s1.find(it)==s1.end())
+                ans2.push_back(it);
         }
         
-        return ans;
+        return {ans1,ans2};
     }
 };
