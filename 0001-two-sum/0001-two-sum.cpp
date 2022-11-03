c3ae9e68b2e6bb9e78527263047786102b96231c
nishant2929 <83642998+nishant2929@users.noreply.github.com>
2022-11-03 16:16:52 +0530

    Time: 26 ms (67.67%), Space: 11 MB (22.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       unordered_map<int,int> mp;
-        vector<int> ans;
+        vector<pair<int,int>> v;
         for(int i=0;i<nums.size();i++){
-            //cout<<mp[nums[i]];
-            if(mp.find(target-nums[i])!=mp.end()){
-                ans.push_back(mp[target-nums[i]]);
-                ans.push_back(i);
+            v.push_back({nums[i],i});
+        }
+        
+        sort(v.begin(),v.end());
+        vector<int> ans;
+        int s=0; int e=nums.size()-1;
+        while(s<=e){
+            if(v[s].first+v[e].first==target){
+                ans.push_back(v[s].second);
+                ans.push_back(v[e].second);
                 break;
-            }  
-             mp[nums[i]]=i;
-    }
+            }
+            if(v[s].first+v[e].first > target){
+                e--;
+            }else{
+                s++;
+            }
+        }
         return ans;
-  }
+        
+    }
 };
-
-// class Solution {
-// public:
-//     vector<int> twoSum(vector<int>& nums, int target) {
-//      unordered_map<int,int> nish;
-//         vector<int> ans;
-//         for(int i=0;i<nums.size();i++){
-//         if(nish.find(target-nums[i])!=nish.end())
-//         {
-//             ans.push_back(i);
-//             ans.push_back(nish[target-nums[i]]);
-//            // return {i,nish[target-nums[i]]};
-//         }
-//             nish[nums[i]]=i;
-//             }
-//         return ans;
-//     }
-// };
