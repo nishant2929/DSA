bf0bb33c8b0cb7bd4e35d1464edb62a52f61eabb
nishant2929 <83642998+nishant2929@users.noreply.github.com>
2022-11-03 15:55:02 +0530

    Time: 18 ms (84.14%), Space: 12.2 MB (8.34%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,36 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//        unordered_map<int,int> mp;
+//         vector<int> ans;
+//         for(int i=0;i<nums.size();i++){
+//           mp[nums[i]]=i;
+//             //cout<<mp[nums[i]];
+//             if(mp.find(target-nums[i])!=mp.end()){
+//                 ans.push_back(mp[target-nums[i]]);
+//                 ans.push_back(i);
+//                 break;
+//                 cout<<"tdytf";
+//             }    
+//     }
+//         return ans;
+//   }
+// };
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+     unordered_map<int,int> nish;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+        if(nish.find(target-nums[i])!=nish.end())
+        {
+            ans.push_back(i);
+            ans.push_back(nish[target-nums[i]]);
+           // return {i,nish[target-nums[i]]};
+        }
+            nish[nums[i]]=i;
+            }
+        return ans;
+    }
+};
