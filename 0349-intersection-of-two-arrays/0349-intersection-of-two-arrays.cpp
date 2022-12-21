6db25389667f50ba5d31232471d612a794bab417
nishant2929 <83642998+nishant2929@users.noreply.github.com>
2022-12-21 12:53:02 +0530

    Time: 11 ms (59.37%), Space: 10.4 MB (37.81%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -3,10 +3,11 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         vector<int> v;
         set<int> s(nums1.begin(),nums1.end());
-        set<int>s2(nums2.begin(),nums2.end());
-       for(auto it : s){
-           if(s2.find(it)!=s2.end()){
+      //  set<int>s2(nums2.begin(),nums2.end());
+       for(auto it : nums2){
+           if(s.find(it)!=s.end()){
                v.push_back(it);
+               s.erase(it);
            }
        }
         return v;
