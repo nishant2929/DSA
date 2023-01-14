cc20d6991822c20575051a24ccc27abcfa93c5f6
nishant2929 <83642998+nishant2929@users.noreply.github.com>
2023-01-14 23:48:06 +0530

    Time: 8 ms (33.07%), Space: 8.1 MB (15.62%) - LeetHub

---
+++ 2451-odd-string-difference/2451-odd-string-difference.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string oddString(vector<string>& words) {
+//         unordered_map<vector<int>v[words[i].size()],int> mp;
+//         for(int i=0;i<words.size();i++){
+//             vector<int>temp;
+//             for(int j=1;j<words[i].size();j++){
+//                 v.push_back(words[i][j]-words[i][j-1]);
+//             }
+//           if(mp.empty()){
+//               mp[temp]+= i;
+//           }else{
+//               if(mp.find(temp)!=mp.end()){
+                  
+//               }
+//           }
+//         }
+        
+       map<vector<int>,vector<string>> mp;
+        for(int i=0;i<words.size();i++){
+             vector<int>temp;
+             for(int j=1;j<words[i].size();j++){
+                 temp.push_back(words[i][j]-words[i][j-1]);
+        
+    }
+         mp[temp].push_back(words[i]);  
+}
+        for(auto it : mp){
+            if(it.second.size()==1){
+                return it.second[0];
+            }
+        }
+        return "";
+    }
+};
