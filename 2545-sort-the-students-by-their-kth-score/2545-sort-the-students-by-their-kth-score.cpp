a90d1046c1fea892761bdd48ffaa21eed8427c7e
nishant2929 <83642998+nishant2929@users.noreply.github.com>
2023-01-22 14:11:18 +0530

    Time: 86 ms (69.23%), Space: 27.7 MB (15.38%) - LeetHub

---
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
+        priority_queue<pair<int,int>>pq;
+        for(int i=0;i<score.size();i++){
+            pq.push({score[i][k],i});
+        }
+       vector<vector<int>> res;
+        while(!pq.empty()){
+            int temp=pq.top().second;
+            pq.pop();
+            res.push_back(score[temp]);
+        }
+        return res;
+    }
+};
