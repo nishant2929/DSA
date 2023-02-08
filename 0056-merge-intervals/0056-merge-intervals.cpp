daad2d87f8eb10993581c07abb3f83b17f5e8610
nishant2929 <83642998+nishant2929@users.noreply.github.com>
2023-02-08 21:37:36 +0530

    Time: 35 ms (92.45%), Space: 19.2 MB (50.68%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,21 +1,49 @@
 class Solution {
 public:
-     vector<vector<int>> merge(vector<vector<int>>& interval) {
-        vector<vector<int>> ans;
-        if(interval.size()==0)return ans;
-        sort(interval.begin(),interval.end());
-        ans.push_back(interval[0]);
-        int j=0;
-        for(int i=1;i<interval.size();i++)
+    vector<vector<int>> merge(vector<vector<int>>& a)
+    {
+        int n=a.size();
+        sort(a.begin(),a.end());
+        vector<vector<int>> r;
+        vector<int> max_overlap;
+        max_overlap.push_back(a[0][0]);
+        max_overlap.push_back(a[0][1]);
+        for(int i=1;i<n;i++)
         {
-            if(ans[j][1]>=interval[i][0])
-                ans[j][1]=max(ans[j][1],interval[i][1]);
+            if(a[i][0]<=max_overlap[1])
+            {
+                max_overlap[1]=max(max_overlap[1],a[i][1]);  // 
+            } 
             else
             {
-                j++;
-                ans.push_back(interval[i]);
+                r.push_back(max_overlap);  //
+                max_overlap[0]=a[i][0];
+                max_overlap[1]=a[i][1];
             }
         }
-        return ans;
+        r.push_back(max_overlap);
+        return r; 
     }
 };
+
+// class Solution {
+// public:
+//      vector<vector<int>> merge(vector<vector<int>>& interval) {
+//         vector<vector<int>> ans;
+//        // if(interval.size()==0)return ans;
+//         sort(interval.begin(),interval.end());
+//         ans.push_back(interval[0]);
+//         int j=0;
+//         for(int i=1;i<interval.size();i++)
+//         {
+//             if(ans[j][1]>=interval[i][0])
+//                 ans[j][1]=max(ans[j][1],interval[i][1]);
+//             else
+//             {
+//                 j++;
+//                 ans.push_back(interval[i]);
+//             }
+//         }
+//         return ans;
+//     }
+// };
