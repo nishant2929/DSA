c683dc00cd1c34b28e133176b84e26ba8e453e51
nishant2929 <83642998+nishant2929@users.noreply.github.com>
2023-02-10 16:32:02 +0530

    Added solution - LeetHub

--- Subset Sums - GFG/subset-sums.cpp
+++ Subset Sums - GFG/subset-sums.cpp
@@ -7,29 +7,29 @@ class Solution
 {
 public:
 
-
-void f(int n,vector<int> arr, int N,int sum,vector<int>&ans){
-    
-    
-    if(n==N){
-        
+void f (vector<int>&arr , int n ,vector<int>&temp,int index,vector<int>&ans){
+    if(index==n){
+        int sum=0;
+        for(auto it : temp){
+            sum+=it;
+        }
         ans.push_back(sum);
         return;
-        
     }
     
-   f(n+1,arr,N,sum+arr[n],ans);
-   
-   f(n+1,arr,N,sum,ans);
+    temp.push_back(arr[index]);
+    f(arr,n,temp,index+1,ans);
+    temp.pop_back();
+    
+    f(arr,n,temp,index+1,ans);
     
 }
     vector<int> subsetSums(vector<int> arr, int N)
     {
-        vector<int> ans;
-       f(0,arr,N,0,ans);
-       //sort(ans.begin(),ans.end());
+        vector<int> temp;
+        vector<int>ans;
+       f(arr,N,temp,0,ans);
        return ans;
-       
     }
 };
 
