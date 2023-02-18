c6ec3c589ee5cc0f80734a2f532775920c402be1
nishant2929 <83642998+nishant2929@users.noreply.github.com>
2023-02-18 12:59:19 +0530

    Time: 2 ms (49.99%), Space: 7.7 MB (85.90%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
-    int f(vector<int>&nums,int n,vector<int>&dp){
-        if(n==0){
-            return nums[n];
-        }
-        if(n<0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        int pick = nums[n]+f(nums,n-2,dp);
-        int notpick = 0 + f(nums,n-1,dp);
-        
-        return dp[n] =max(pick,notpick);
-    }
     int rob(vector<int>& nums) {
-        int n = nums.size()-1;
-        vector<int> dp(n+2,-1);
-        return f(nums,n,dp);
-        
+        int n =nums.size();
+        vector<int>dp(n+1,-1);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            int take = nums[i];
+            if(i>1){
+               take = take + dp[i-2];
+            }
+            int ntake = 0 + dp[i-1];
+            dp[i]= max(take,ntake);
+        }
+        return dp[n-1];
     }
 };
+
+// dp[0]= arr[0];
+    
+//     for(int i=1 ;i<n; i++){
+//         int pick = arr[i];
+//         if(i>1)
+//             pick += dp[i-2];
+//         int nonPick = 0+ dp[i-1];
+        
+//         dp[i]= max(pick, nonPick);
+//     }
+    
+    
+//     return dp[n-1];
