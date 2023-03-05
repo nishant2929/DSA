7621d835702dfbd68f6c66f6d6466bf8fcea284e
nishant2929 <83642998+nishant2929@users.noreply.github.com>
2023-03-05 20:45:11 +0530

    Time: 2 ms (47.72%), Space: 6.3 MB (55.62%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
     bool isValid(string s) {
-       stack<char> store;
+       stack<int>st;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
-                store.push(s[i]);
-            }
-            else {
-                if(store.size()==0){
-                    return false;
-                }
-                if(s[i]==')'){
-                if(store.top()!='('){
-                    return false;
-                }
-                store.pop();
-            }else if(s[i]==']'){
-                if(store.top()!='['){
-                    return false;
-                }
-                store.pop();
+            if(st.size()==0){
+                st.push(s[i]);
             }else{
-                if(store.top()!='{'){
-                    return false;
+                if(st.top()=='(' and s[i]==')' || st.top()=='[' and s[i]==']' || st.top()=='{' and s[i]=='}'){
+                    st.pop();
+                }else{
+                    st.push(s[i]);
                 }
-                store.pop();
+                    
             }
         }
-        }
-        if(store.size()==0){
+        if(st.size()==0){
             return true;
         }
         return false;
