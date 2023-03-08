808d62d44192544e17373c383cc749fd78c3edc4
nishant2929 <83642998+nishant2929@users.noreply.github.com>
2023-03-08 23:03:51 +0530

    Time: 13 ms (37.12%), Space: 11.5 MB (27.08%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,42 +1,45 @@
-bool comp(string&a ,string&b){
-    return a+b>b+a;
-}
-
+  bool lar(string&a ,string&b){
+        return a+b>b+a;
+    }
 class Solution {
 public:
+  
     string largestNumber(vector<int>& nums) {
-        vector<string> v;
-        for(auto it: nums){
-            string s =to_string(it);
-            v.push_back(s);
+        vector<string> s;
+        for(int i=0;i<nums.size();i++){
+            s.push_back(to_string(nums[i]));
         }
-        sort(v.begin(),v.end(),comp);
+        
+        sort(s.begin(),s.end(),lar);
         string ans="";
-        for(int i=0;i<v.size();i++){
-            ans+=v[i];
+      //  vector<int>ans;
+        for(auto it : s){
+           ans+=it;
         }
-        return ans[0]=='0'?"0":ans;
+        if(ans[0]=='0'){
+            return "0";
+        }
+        return ans;
     }
 };
 
-// bool compare(string a,string b){
-//     return a+b > b+a;    
+// bool comp(string&a ,string&b){
+//     return a+b>b+a;
 // }
 
 // class Solution {
 // public:
 //     string largestNumber(vector<int>& nums) {
-	
-//         vector<string> container;
-		
-//         for(int i : nums)  container.push_back(to_string(i));
-        
-//         sort(container.begin(),container.end(),compare);
-        
-//         string result;
-        
-//         for(int i=0;i<container.size();i++)  result+=container[i];
-        
-//         return result[0]=='0'?"0":result;
+//         vector<string> v;
+//         for(auto it: nums){
+//             string s =to_string(it);
+//             v.push_back(s);
+//         }
+//         sort(v.begin(),v.end(),comp);
+//         string ans="";
+//         for(int i=0;i<v.size();i++){
+//             ans+=v[i];
+//         }
+//         return ans[0]=='0'?"0":ans;
 //     }
 // };
