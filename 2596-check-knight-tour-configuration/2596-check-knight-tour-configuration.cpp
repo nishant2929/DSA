2d18d60cad57c54e27cdc580dc5623d40c26422a
nishant2929 <83642998+nishant2929@users.noreply.github.com>
2023-03-25 03:51:40 +0530

    Time: 18 ms (68.33%), Space: 28.2 MB (48.35%) - LeetHub

--- 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     bool checkValidGrid(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int p=n*n-1;
-
-        if(grid[0][0]!=0) return 0;
-
-        vector<pair<int,int>> v(p+1);
-
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++) v[grid[i][j]]={i,j};
+         map<int,pair<int,int>> mp;
+        if(grid[0][0]!=0){
+            return false;
         }
-
-        bool flag=0;
-        for(int i=1;i<=p;i++)
-        {
-            
-            if(!((abs(v[i].first-v[i-1].first)==2 && abs(v[i].second-v[i-1].second)==1)) && !((abs(v[i].first-v[i-1].first)==1 && abs(v[i].second-v[i-1].second)==2))) {flag=1; break;}
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                mp[grid[i][j]] = {i,j};
+            }
         }
-        return flag==0;
-       
+        
+        vector<pair<int,int>> ans;
+        for(auto it : mp){
+            ans.push_back(it.second);
+        }
+        
+        for(int i=1; i<ans.size(); i++){
+            if(abs(ans[i].first - ans[i-1].first) + abs(ans[i].second - ans[i-1].second) != 3 or
+               (abs(ans[i].first - ans[i-1].first) == 3 && abs(ans[i].second - ans[i-1].second) == 0) or
+               (abs(ans[i].first - ans[i-1].first) == 0 && abs(ans[i].second - ans[i-1].second) == 3)){
+                return false;
+            }
+        }
+        return true;
     }
 };
